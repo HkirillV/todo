c2fc707e14ea1f4ac56b14e2e6a6096890ed98fc
HKIRILLV <khlebov79@gmail.com>
2024-06-23 12:11:04 +0300

    fix: fix project

--- src/js/categoryTask.js
+++ src/js/categoryTask.js
@@ -5,15 +5,15 @@ const addTaskFormSelectElement = document.querySelector('.add-task-form__select'
 
 const getCategoryTask = getCacheToCategoryTask()
 
-let categoryTasks = getCategoryTask.length > 0 ? getCategoryTask : await taskApi.getTask('category')
+const categoryTasks = getCategoryTask.length > 0 ? getCategoryTask : await taskApi.getTask('category')
 
 const renderCategoryTask = () => {
 
   categoryElement.innerHTML = categoryTasks.reduce((acc, el) => {
-    const {id, title, img, tasks} = el
+    const {id, title, imgSrc, tasks} = el
     const task = `
       <div class="category__task" data-id="${id}">
-      <img class="category__task-image" src="/src/img/${img}" width="70" height="70" loading="lazy" alt="work">
+      <img class="category__task-image" src="/src/img/${imgSrc}" width="70" height="70" loading="lazy" alt="work">
       <h4 class="category__task-title">${title}</h4>
       <span class="category__task-count">Tasks ${tasks}</span>
     </div>
